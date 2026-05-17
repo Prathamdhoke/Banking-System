5ac2f3c2a38ac5f4626c929d16bcde2bf173edf0
Pratham Dhoke <prathamcode01@gmail.com>
2026-05-17 12:37:57 +0530

    Added accounts table with banking constraints

--- schema.sql
+++ schema.sql
@@ -24,3 +24,28 @@ CREATE TABLE branches(
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE accounts(
+	account_id INT PRIMARY KEY AUTO_INCREMENT,
+    account_number VARCHAR(20) UNIQUE NOT NULL,
+    customer_id INT NOT NULL,
+    branch_id INT NOT NULL,
+    account_type ENUM('Saving','Current') NOT NULL,
+    balance DECIMAL(12,2) NOT NULL DEFAULT 0,
+    minimum_balance DECIMAL(12,2) DEFAULT 1000,
+    status ENUM('Active','Frozen','Closed') DEFAULT 'Active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    CONSTRAINT chk_balance
+    CHECK (balance >= 0),
+    
+    CONSTRAINT fk_customer
+    FOREIGN KEY (customer_id)
+    REFERENCES customers (customer_id),
+    
+    CONSTRAINT fk_branch
+    FOREIGN KEY (branch_id)
+    REFERENCES branches(branch_id)
+);
+
+
